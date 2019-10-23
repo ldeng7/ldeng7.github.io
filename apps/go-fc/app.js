c978f0b1bbc8eb5cef79208d705aa65a33136e26
Lynn Deng <beednln@163.com>
2019-10-24 03:20:23 +0800

    update go-fc

--- apps/go-fc/app.js
+++ apps/go-fc/app.js
@@ -1,26 +1,36 @@
-let goMemArr, fbOffset, fbSize, canvasCtx, canvasData
+const screenWidth = 272
+const screenHeight = 240
+let goMemArr
+let canvasCtx, canvasData, fbOffset
 
+window.goFuncs = {}
 window.copyFromJsArr = (arr, ptr) => {
   goMemArr.set(arr, ptr)
 }
-window.setFrameBuffer = (ptr, sz) => {
+window.setFrameBuffer = (ptr) => {
   fbOffset = ptr
-  fbSize = sz
 }
 window.updateScreen = () => {
-  canvasData.data.set(goMemArr.slice(fbOffset, fbOffset + fbSize))
+  canvasData.data.set(goMemArr.slice(fbOffset, fbOffset + screenWidth * screenHeight * 4))
   canvasCtx.putImageData(canvasData, 0, 0)
 }
-window.goFuncs = {}
 
-const go = new Go()
-WebAssembly.instantiateStreaming(fetch("go_main.wasm"), go.importObject).
-  then(res => {
-    goMemArr = new Uint8Array(res.instance.exports.mem.buffer)
-    go.run(res.instance)
-  })
+let onRomFileOpened = event => {
+  let fileArr = new Uint8Array(event.target.result)
+  let ret = window.goFuncs.start(fileArr, fileArr.length)
+  if (ret === true) {
+    ret = "running."
+    document.getElementById("file-input").hidden = true
+    onEmuStart()
+  }
+  document.getElementById("msg").innerText = ret
+}
 
 let onEmuStart = () => {
+	console.log("fuck")
+  canvasCtx = document.getElementById("screen").getContext("2d", {alpha: false})
+  canvasData = canvasCtx.createImageData(screenWidth, screenHeight)
+
   document.onkeydown = event => {
     window.goFuncs.onKey(event.code, true)
   }
@@ -29,24 +39,19 @@ let onEmuStart = () => {
   }
 }
 
+const go = new Go()
+WebAssembly.instantiateStreaming(fetch("go_main.wasm"), go.importObject).
+  then(res => {
+    goMemArr = new Uint8Array(res.instance.exports.mem.buffer)
+    go.run(res.instance)
+  })
 window.onload = () => {
   let fileElem = document.getElementById("file-input")
   fileElem.addEventListener("change", event => {
     let reader = new FileReader()
-    reader.onload = event => {
-      let fileArr = new Uint8Array(event.target.result)
-      let ret = window.goFuncs.start(fileArr, fileArr.length)
-      if (ret === true) {
-        ret = "running."
-        fileElem.hidden = true
-        onEmuStart()
-      }
-      document.getElementById("msg").innerText = ret
-    }
+    reader.onload = onRomFileOpened
     if (event.target.files.length > 0) {
       reader.readAsArrayBuffer(event.target.files[0])
     }
   })
-  canvasCtx = document.getElementById("screen").getContext("2d", {alpha: false})
-  canvasData = canvasCtx.createImageData(272, 240)
 }
